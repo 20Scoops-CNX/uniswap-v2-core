191e724f8ef2658ead4b3dbb0c202d90b3a32e2c
wimkung <wimkung@gmail.com>
2020-10-08 12:00:01 +0700

    update network

--- networks.js
+++ networks.js
@@ -10,8 +10,8 @@ module.exports = {
       provider: function () {
         return new HDWalletProvider({
           mnemonic,
-          provider: `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_KEY}`,
-          addressIndex: process.env.ACCOUNT_INDEX || 0
+          providerOrUrl: `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_KEY}`,
+          addressIndex: Number(process.env.ACCOUNT_INDEX || 0)
         });
       },
       network_id: 1
@@ -28,8 +28,8 @@ module.exports = {
       provider: function () {
         return new HDWalletProvider({
           mnemonic,
-          provider: `https://ropsten.infura.io/v3/${process.env.INFURA_PROJECT_KEY}`,
-          addressIndex: process.env.ACCOUNT_INDEX || 0
+          providerOrUrl: `https://ropsten.infura.io/v3/${process.env.INFURA_PROJECT_KEY}`,
+          addressIndex: Number(process.env.ACCOUNT_INDEX || 0)
         });
       },
       network_id: 3
@@ -38,8 +38,8 @@ module.exports = {
       provider: function () {
         return new HDWalletProvider({
           mnemonic,
-          provider: `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_KEY}`,
-          addressIndex: process.env.ACCOUNT_INDEX || 0
+          providerOrUrl: `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_KEY}`,
+          addressIndex: Number(process.env.ACCOUNT_INDEX || 0)
         });
       },
       network_id: 4
@@ -48,8 +48,8 @@ module.exports = {
       provider: function () {
         return new HDWalletProvider({
           mnemonic,
-          provider: process.env.OFFICE_PROVIDER || `http://127.0.0.1:7545`,
-          addressIndex: process.env.ACCOUNT_INDEX || 0
+          providerOrUrl: process.env.OFFICE_PROVIDER || `http://127.0.0.1:7545`,
+          addressIndex: Number(process.env.ACCOUNT_INDEX || 0)
         });
       },
       network_id: "*"
